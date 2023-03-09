0bfca36f469c4a92e2636e8877b5344bf1462bb9
Mo8 <juliancautela@gmail.com>
2023-03-09 15:13:10 +0100

    fix + no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Connect from "./pages/Connect";
 import Form from "./pages/Form";
-import "./App.css";
 import Rdvs from "./pages/Rdvs";
 import { useEffect, useState } from "react";
 
@@ -19,23 +18,21 @@ function App() {
   return (
     <div>
       <div>
-        {deferredPrompt ? (
+        {deferredPrompt && (
           <button
             onClick={async () => {
               if (deferredPrompt !== null) {
                 deferredPrompt.prompt();
                 const { outcome } = await deferredPrompt.userChoice;
                 if (outcome === "accepted") {
-                  deferredPrompt = null;
+                  setDeferredPrompt(null);
                 }
               }
             }}
           >
             Install
           </button>
-        ) : (
-          "No install possible"
-        )}
+        ) }
       </div>
       <Routes>
         <Route path="/" element={<Connect />} />
