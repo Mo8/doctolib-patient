de8cfdc430538c74c0a6297d49f1a2de3d89c1af
Mo8 <juliancautela@gmail.com>
2023-03-09 14:57:42 +0100

    install button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
-import { Route,Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Connect from "./pages/Connect";
 import Form from "./pages/Form";
-import './App.css'
+import "./App.css";
 import Rdvs from "./pages/Rdvs";
+import { useEffect, useState } from "react";
 
-function App(){
-
-    return (<>
-    <Routes>
-        <Route path="/" element={<Connect/>} />        
-        <Route path="/form" element={<Form/>} />              
-        <Route path="/rdvs" element={<Rdvs/>} />
-    </Routes>
-    </>)
+function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  useEffect(() => {
+    window.addEventListener(
+      "beforeinstallprompt",
+      (e) => {
+        setDeferredPrompt(e);
+      },
+      []
+    );
+  });
+  return (
+    <div>
+      <div>
+        {deferredPrompt ? (
+          <button
+            onClick={async () => {
+              if (deferredPrompt !== null) {
+                deferredPrompt.prompt();
+                const { outcome } = await deferredPrompt.userChoice;
+                if (outcome === "accepted") {
+                  deferredPrompt = null;
+                }
+              }
+            }}
+          >
+            Install
+          </button>
+        ) : (
+          "No install possible"
+        )}
+      </div>
+      <Routes>
+        <Route path="/" element={<Connect />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/rdvs" element={<Rdvs />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
